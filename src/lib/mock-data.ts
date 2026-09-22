c337c54af3355c46150e546d4ad43f0a5472af89
Traipop <traipopwichiansarn@gmail.com>
2026-09-22 13:47:45 +0700

    finished

--- src/lib/mock-data.ts
+++ src/lib/mock-data.ts
@@ -75,5 +75,5 @@ export const currentStudent = students.find(
 export const currentUser = {
   nickname: "Ice",
   role: "STUDENT" as "STUDENT" | "ADMIN",
-  avatar: "/public/profile.jpg", // วางรูปไว้ที่ public/
+  avatar: "./public/profile.jpg", // วางรูปไว้ที่ public/
 };
